b84852f85291765a8cfb7379c7cdc1938f49db56
Tomas Bernotas <email@email.com>
2025-01-08 20:59:47 +0200

    Pakoreguotas JS kodas

--- app.js
+++ app.js
@@ -11,24 +11,24 @@ const passwordOutput = document.getElementById('passwordOutput');
 const copyButton = document.getElementById('copyButton');
 
 
-const generatePassword = (length, includeUppercase, includeNumbers, includeSymbols) => {
+const generatePassword = (length, Uppercase, Numbers, Symbols) => {
   const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
   const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   const numbers = '0123456789';
   const symbols = '!@#$%^&*()_+[]{}|;:,.<>?';
 
-  let characterPool = lowerCase; //visi simboliai tik mazosios raides
-  if (includeUppercase) characterPool += upperCase;// prideda pazymejus didziasias raides
-  if (includeNumbers) characterPool += numbers;// prideda pazymejus sk.
-  if (includeSymbols) characterPool += symbols;// prideda pazymejus simbolius
+  let container = lowerCase; //visi simboliai tik mazosios raides
+  if (Uppercase) container += upperCase;// prideda pazymejus didziasias raides
+  if (Numbers) container += numbers;// prideda pazymejus sk.
+  if (Symbols) container += symbols;// prideda pazymejus simbolius
 
-  if (characterPool.length === 0) return 'Please select at least one option.';
+  if (container.length === 0) return 'Please select at least one option.';
 
   //sugeneruojamas slaptazodis is atsitiktiniu simboliu ir raidziu
   let password = '';//tarkim slaptazodis tuscias stringas
   for (let i = 0; i < length; i++) {
-    const randomIndex = Math.floor(Math.random() * characterPool.length);
-    password += characterPool[randomIndex];
+    const randomIndex = Math.floor(Math.random() * container.length);
+    password += container[randomIndex];
   }
 
   return password;
@@ -37,14 +37,14 @@ const generatePassword = (length, includeUppercase, includeNumbers, includeSymbo
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   const length = +document.getElementById('number').value;
-  const includeUppercase = document.getElementById('uppercase').checked;
-  const includeNumbers = document.getElementById('numbers').checked;
-  const includeSymbols = document.getElementById('symbols').checked;
+  const Uppercase = document.getElementById('uppercase').checked;
+  const Numbers = document.getElementById('numbers').checked;
+  const Symbols = document.getElementById('symbols').checked;
 
-  const password = generatePassword(length, includeUppercase, includeNumbers, includeSymbols);
+  const password = generatePassword(length, Uppercase, Numbers, Symbols);
   passwordOutput.value = password;
 });
-//event liseneris mygtukui
+//event liseneris mygtukui nusikopijuoti slaptazodi vienu paspaudimu
 copyButton.addEventListener('click', () => {
   const password = passwordOutput.value;
   navigator.clipboard.writeText(password);
